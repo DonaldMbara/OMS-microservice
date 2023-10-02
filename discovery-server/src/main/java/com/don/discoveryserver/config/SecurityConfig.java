ea2d6ac8177b4c78e5680212e5a12348b9014f03
DonaldMbara <donaldmbara@DonaldMbaras-MacBook-Pro.local>
2023-10-02 14:13:56 +0200

    security config in the api gateway and discovery-server

---
+++ discovery-server/src/main/java/com/don/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.don.discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        return httpSecurity.build();
+    }
+}
