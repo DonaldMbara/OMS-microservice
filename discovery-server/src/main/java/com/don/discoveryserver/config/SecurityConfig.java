fd550ebb12298d1fcd97cfbcabc40e4099a461d8
DonaldMbara <donaldmbara@DonaldMbaras-MacBook-Pro.local>
2023-10-05 16:41:20 +0200

    updated few things in the config

--- discovery-server/src/main/java/com/don/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/don/discoveryserver/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.don.discoveryserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
