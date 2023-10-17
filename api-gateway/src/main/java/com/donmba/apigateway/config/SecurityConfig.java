c084001daecb3043509f790df3a289b19ef144ed
DonaldMbara <donaldmbara@gmail.com>
2023-10-17 22:25:27 +0200

    add cors config

--- api-gateway/src/main/java/com/donmba/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/donmba/apigateway/config/SecurityConfig.java
@@ -1,18 +1,21 @@
-package com.donmba.apigateway.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity security){
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity security) {
         security.csrf(csrf -> csrf.disable())
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**")
@@ -20,6 +23,27 @@ public class SecurityConfig {
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(oath2 -> oath2.jwt(Customizer.withDefaults()));
+
         return security.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public WebFilter corsFilter() {
+        CorsWebFilter corsFilter = new CorsWebFilter(corsConfigurationSource());
+        return corsFilter;
     }
 }
