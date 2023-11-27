4521504b7513121a6502c17ede476fa54f99be0c
DonaldMbara <donaldmbara@gmail.com>
2023-11-27 16:32:00 +0200

    updated endpoint

--- order-service/src/main/java/com/donmba/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/donmba/orderservice/repository/OrderRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-    @Query("SELECT o FROM Product o WHERE o.customer_id = :customer_id")
+
+    @Query("SELECT o FROM Order o WHERE o.customer_id = :customer_id")
     List<Order> findByCustomerId(@Param("customer_id") int customer_id);
 }
